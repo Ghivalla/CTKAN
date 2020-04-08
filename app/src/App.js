868ac3eed9e93a214295b00627a287adcd35e8df
Mathieu Enault <mathieu.enault@coding-academy.fr>
2020-04-08 20:14:15 +0200

    connexion page

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Login from "./Login.js";
+import Inscriptions from "./Inscriptions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const email = target.email;
+    const password = target.password;
+    this.setState({
+      [email]: target.value,
+      [password]: target.value,
+    });
+  };
+
+  render() {
+    return (
+      <section className="App">
+        <Inscriptions />
+      </section>
+    );
+  }
 }
 
 export default App;
